964156581b4e7b55bf2a82c368626f86db16bb80
aPPORV <apporv1999@gmail.com>
2024-12-01 17:47:01 +0530

    Second changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-export const GOOGLE_API_KEY = "AIzaSyAiBTPun96Nuw9oKALiKf_666glaf_IdkE";
+import { googleApiCredentials } from "./password";
+export const GOOGLE_API_KEY = googleApiCredentials;
 
 export const YOUTUBE_VIDEO_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
@@ -7,6 +8,6 @@ export const YOUTUBE_VIDEO_API =
 export const YOUTUBE_SEARCH_API =
   "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
-  // "https://corsproxy.io/?http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+// "https://corsproxy.io/?http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const LIVE_CHAT_COUNT = 25;
